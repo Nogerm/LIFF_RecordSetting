b0c87caf6f44a9cea5956aae1a06ce287c02e48f
Andy_CW_Wu <andy_cw_wu@wistron.com>
2019-08-07 10:46:00 +0800

    init commit

---
+++ recordShow.js
@@ -0,0 +1,40 @@
+const hostURL = "https://script.google.com/macros/s/AKfycbyQwaNfRrnyBB4kCOvdMgUw_o6v8Z_lNUDqjNCT5Uo-dPKBvZ0/exec";
+
+//init
+window.onload = function (e) {
+
+  liff.init(
+    data => {
+      // Now you can call LIFF API
+      initializeApp(data);
+    },
+    err => {
+      // LIFF initialization failed
+      alert("init fail");
+
+      //show/hide element
+      let div_loading = document.getElementById("loading");
+      div_loading.className = "ui inverted dimmer";
+    }
+  );
+};
+
+function initializeApp(data) {
+  //check user permission
+  const query_url = hostURL + "?type=record&lineId=" + data.context.userId;
+  axios.get(query_url)
+  .then(response => {
+    // Success
+
+    //show/hide element
+    let div_loading = document.getElementById("loading");
+    div_loading.className = "ui inverted dimmer";
+
+  })
+  .catch(error => {
+    // Error
+    console.log(error);
+    alert(error);
+  });
+}
+
