54e24bcd83a15b3966538e98948eb8c7f02c9ab8
Andy CW Wu <andy_cw_wu@wistron.com>
2019-08-09 16:08:45 +0800

    add apply function

--- recordShow.js
+++ recordShow.js
@@ -1,4 +1,5 @@
 const hostURL = "https://script.google.com/macros/s/AKfycbyQwaNfRrnyBB4kCOvdMgUw_o6v8Z_lNUDqjNCT5Uo-dPKBvZ0/exec";
+let lineId = "";
 
 //init
 window.onload = function (e) {
@@ -20,6 +21,7 @@ window.onload = function (e) {
 };
 
 function initializeApp(data) {
+  lineId = data.context.userId;
   //check user permission
   const query_url = hostURL + "?type=record&lineId=" + data.context.userId;
   axios.get(query_url)
@@ -35,6 +37,44 @@ function initializeApp(data) {
     // Error
     console.log(error);
     alert(error);
+  });
+}
+
+function applyAuth() {
+
+  const postData = {
+    type: 'apply_auth',
+    userName: "Name",
+    groupName: "Group",
+    lineId: lineId
+  };
+  //alert("post data: " + postData);
+
+  $.ajax({
+    url: hostURL,
+    type: "POST",
+    datatype: "json",
+    data: postData,
+    success: function (res, status) {
+      alert("server result: " + JSON.stringify(res) + "\nstatus: " + status);
+      btn.className = "fluid ui button";
+      swal.fire({
+        title: '回報成功',
+        text: '點擊確定關閉視窗',
+        type: 'success',
+        onClose: () => {
+          liff.closeWindow();
+        }
+      });
+    },
+    error: function(xhr, ajaxOptions, thrownError) {
+      btn.className = "fluid ui button";
+      swal.fire({
+        title: '錯誤',
+        text: "post error: " + xhr.responseText + "\najaxOptions: " + ajaxOptions + "\nthrownError: " + thrownError,
+        type: 'error'
+      });
+    }
   });
 }
 
