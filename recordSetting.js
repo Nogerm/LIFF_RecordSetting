e9304539c42105feb8e50d623c5f99a5d49860b3
Andy CW Wu <andy_cw_wu@wistron.com>
2019-10-03 18:19:07 +0800

    remove global variables

--- recordSetting.js
+++ recordSetting.js
@@ -1,15 +1,9 @@
 const hostURL = "https://script.google.com/macros/s/AKfycbyQwaNfRrnyBB4kCOvdMgUw_o6v8Z_lNUDqjNCT5Uo-dPKBvZ0/exec";
-let lineId = "";
-let inputName = "";
-let inputGroup = "";
 
 //init
 window.onload = function (e) {
 
   //init data
-  lineId = "";
-  inputName = "";
-  inputGroup = "";
   document.getElementById("formName").value = "";
   document.getElementById("formGroup").value = "";
 
@@ -30,23 +24,10 @@ window.onload = function (e) {
 };
 
 function initializeApp(data) {
-  lineId = data.context.userId;
-  //check user permission
-  const query_url = hostURL + "?type=record&lineId=" + data.context.userId;
-  axios.get(query_url)
-  .then(response => {
-    // Success
 
-    //show/hide element
-    let div_loading = document.getElementById("loading");
-    div_loading.className = "ui inverted dimmer";
-
-  })
-  .catch(error => {
-    // Error
-    console.log(error);
-    alert(error);
-  });
+  //show/hide element
+  let div_loading = document.getElementById("loading");
+  div_loading.className = "ui inverted dimmer";
 }
 
 function nameChange(element) {
@@ -61,10 +42,10 @@ function groupChange(element) {
 
 function checkBothFormFilled () {
   let formName  = document.getElementById("formName");
-  inputName = formName.value;
+  let inputName = formName.value;
 
   let formGroup = document.getElementById("formGroup");
-  inputGroup = formGroup.value;
+  let inputGroup = formGroup.value;
 
   //button enable/disable
   let button = document.getElementById("btnApply");
@@ -73,52 +54,64 @@ function checkBothFormFilled () {
 }
 
 function applyAuth() {
-  //add loading to button
-  const btn = document.getElementById("btnApply");
-  btn.className = "fluid ui loading submit button";
 
-  const postData = {
-    type: 'apply_auth',
-    userName: inputName,
-    groupName: inputGroup,
-    lineId: lineId
-  };
-  //alert("post data: " + postData);
+  liff.getProfile()
+  .then(profile => {
+    //add loading to button
+    const btn = document.getElementById("btnApply");
+    btn.className = "fluid ui loading submit button";
 
-  $.ajax({
-    url: hostURL,
-    type: "POST",
-    datatype: "json",
-    data: postData,
-    success: function (res, status) {
-      //alert("server result: " + JSON.stringify(res) + "\nstatus: " + status);
-      btn.className = "fluid ui submit button";
-      
-      if(res.status === 200) {
+    const postData = {
+      type: 'apply_auth',
+      userName: document.getElementById("formName").value,
+      groupName: document.getElementById("formGroup").value,
+      lineId: profile.userId,
+    };
+    //alert("post data: " + postData);
+  
+    $.ajax({
+      url: hostURL,
+      type: "POST",
+      datatype: "json",
+      data: postData,
+      success: function (res, status) {
+        //alert("server result: " + JSON.stringify(res) + "\nstatus: " + status);
+        btn.className = "fluid ui submit button";
+        
+        if(res.status === 200) {
+          swal.fire({
+            title: '申請成功',
+            text: '點擊確定關閉視窗',
+            type: 'success',
+            onClose: () => {
+              liff.closeWindow();
+            }
+          });
+        } else if (res.status === 202) {
+          swal.fire({
+            title: '申請重複',
+            text: '資料審查中，請耐心等候',
+            type: 'error'
+          });
+        }
+      },
+      error: function(xhr, ajaxOptions, thrownError) {
+        btn.className = "fluid ui submit button";
         swal.fire({
-          title: '申請成功',
-          text: '點擊確定關閉視窗',
-          type: 'success',
-          onClose: () => {
-            liff.closeWindow();
-          }
-        });
-      } else if (res.status === 202) {
-        swal.fire({
-          title: '申請重複',
-          text: '資料審查中，請耐心等候',
+          title: '錯誤',
+          text: "post error: " + xhr.responseText + "\najaxOptions: " + ajaxOptions + "\nthrownError: " + thrownError,
           type: 'error'
         });
       }
-    },
-    error: function(xhr, ajaxOptions, thrownError) {
-      btn.className = "fluid ui submit button";
-      swal.fire({
-        title: '錯誤',
-        text: "post error: " + xhr.responseText + "\najaxOptions: " + ajaxOptions + "\nthrownError: " + thrownError,
-        type: 'error'
-      });
-    }
+    });
+  })
+  .catch((err) => {
+    console.log('error', err);
+    swal.fire({
+      title: '錯誤',
+      text: err,
+      type: 'error'
+    });
   });
 }
 
